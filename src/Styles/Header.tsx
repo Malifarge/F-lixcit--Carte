c72718acc44fc731b0c41b239c2f4184eefacc77
Savage Blood <malifarge.thomas9@gmail.com>
2023-03-09 17:38:54 +0100

    entête transparence bord

--- src/Styles/Header.tsx
+++ src/Styles/Header.tsx
@@ -4,7 +4,7 @@ export const HeaderStyle= styled.header`
     display:flex;
     justify-Content: space-between;
     padding:10px;
-    border-bottom: 2px solid ${({color})=>color};
+    border-bottom: 2px solid ${({color})=>color}90;
 
     img{
         width:50px;
